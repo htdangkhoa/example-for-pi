0b6983cad184e69523a20593c993fac946331314
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2022-04-25 00:25:23 +0700

    fix nginx

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ const app = express();
 
 app.all('/', (req, res) => res.send('Hello World!'));
 
+app.all('/name', (req, res) => res.send(req.query));
+
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
